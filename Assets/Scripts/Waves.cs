50eea7b3e2fb1bc4d601d5c21ec441ae384c1472
Lucas <imac@iMac-de-iMac-2.local>
2018-02-21 16:27:45 -0300

    Unleash Button

---
+++ Assets/Scripts/Waves.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour {
+
+	public GameObject enemy;
+	public Transform spawnPoint;
+
+	public void unleashWave()
+	{
+		Instantiate (enemy, spawnPoint.position, spawnPoint.rotation);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
